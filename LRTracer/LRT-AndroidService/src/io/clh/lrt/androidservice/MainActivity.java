1e274f149c17071105bc71fc40d1357b59b03d66
Ross Nordstrom <ross.nordstrom@flukenetworks.com>
2013-07-09 21:28:19 -0600

    working on service like madmen

--- LRTracer/LRT-AndroidService/src/io/clh/lrt/androidservice/MainActivity.java
+++ LRTracer/LRT-AndroidService/src/io/clh/lrt/androidservice/MainActivity.java
@@ -1,31 +1,52 @@
 package io.clh.lrt.androidservice;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	  
+
 	// Debug verbosity
 	private static final String TAG = "lrt_main";
-	private static int LOGLEVEL 			= 3;	// 0 = None, 1 = Warn, 2 = Debug, 3 = Verbose
-	private static boolean WARN 			= LOGLEVEL > 0;
-	private static boolean DEBUG 			= LOGLEVEL > 1;
-	private static boolean VERBOSE			= LOGLEVEL > 2;
+	private static int LOGLEVEL = 3; // 0 = None, 1 = Warn, 2 = Debug, 3 =
+										// Verbose
+	private static boolean WARN = LOGLEVEL > 0;
+	private static boolean DEBUG = LOGLEVEL > 1;
+	private static boolean VERBOSE = LOGLEVEL > 2;
 
+	private TextView dbgView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		Log.i(TAG, "MainActivity#Create");
 		startService(new Intent(MainActivity.this, TraceListenerService.class));
 
 		sendBroadcast("Started the main LRT activity");
 		sendBroadcast("Second broadcast");
+
+		Button button = (Button) findViewById(R.id.btnRefresh);
+
+		button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dumpQueue();
+			}
+		});
+
+		dbgView = (TextView) findViewById(R.id.dbg);
 	}
 
 	@Override
@@ -36,11 +57,32 @@ public class MainActivity extends Activity {
 	}
 
 	private void sendBroadcast(String msg) {
-    	Log.d(TAG, "Send broadcast: "+msg);
-    	Intent connBroadcastIntent = new Intent(TraceListenerService.ACTION_LRT_TEST);
-    	connBroadcastIntent.putExtra("message", msg);
-    	connBroadcastIntent.setPackage("io.clh.lrt.androidservice");		// Limit who sees the broadcast
-    	sendBroadcast(connBroadcastIntent);
-    	Log.d(TAG, "Sent.");
+		Log.d(TAG, "Send broadcast: " + msg);
+		Intent connBroadcastIntent = new Intent(
+				TraceListenerService.ACTION_LRT_TEST);
+		connBroadcastIntent.putExtra("message", msg);
+		connBroadcastIntent.setPackage("io.clh.lrt.androidservice"); // Limit
+																		// who
+																		// sees
+																		// the
+																		// broadcast
+		sendBroadcast(connBroadcastIntent);
+		Log.d(TAG, "Sent.");
+	}
+
+	public void teddyBear() {
+		Log.i(TAG, "I'm safe!");
+	}
+
+	public void dumpQueue() {
+		Log.w(TAG, "DUMP DAT QUEUE");
+		LinkedList<String> dataBuffer = TraceListenerService.getQueue();
+		String stringBuffer = "";
+		if (dataBuffer != null) {
+			for (String string : dataBuffer) {
+				stringBuffer += string + "\n";
+			}
+		}
+		dbgView.setText(stringBuffer);
 	}
 }
--- LRTracer/LRT-AndroidService/src/io/clh/lrt/androidservice/TraceListenerService.java
+++ LRTracer/LRT-AndroidService/src/io/clh/lrt/androidservice/TraceListenerService.java
@@ -16,6 +16,9 @@
 
 package io.clh.lrt.androidservice;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -33,151 +36,198 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class TraceListenerService extends Service {
-	  private Looper mServiceLooper;
-	  private ServiceHandler mServiceHandler;
-	  
+	private Looper mServiceLooper;
+	private ServiceHandler mServiceHandler;
+	private LinkedList<String> mDataBuffer = new LinkedList<String>();
+
 	// Debug verbosity
 	private static final String TAG = "lrt_androidservice";
-	private static int LOGLEVEL 			= 3;	// 0 = None, 1 = Warn, 2 = Debug, 3 = Verbose
-	private static boolean WARN 			= LOGLEVEL > 0;
-	private static boolean DEBUG 			= LOGLEVEL > 1;
-	private static boolean VERBOSE			= LOGLEVEL > 2;
+	private static int LOGLEVEL = 3; // 0 = None, 1 = Warn, 2 = Debug, 3 =
+										// Verbose
+	private static boolean WARN = LOGLEVEL > 0;
+	private static boolean DEBUG = LOGLEVEL > 1;
+	private static boolean VERBOSE = LOGLEVEL > 2;
 
 	// Intents handled in this Service
 	public static final String ACTION_LRT_TEST = "io.clh.lrt.action.test";
 
 	// Broadcasts we make and receive
 	public static final String BROADCAST_LRT_TEST = "io.clh.lrt.broadcast.test";
-	
-	  // Handler that receives messages from the thread
-	  private final class ServiceHandler extends Handler {
+
+	// Handler that receives messages from the thread
+	private final class ServiceHandler extends Handler {
 		private static final String TAG = "lrt_androidservice";
+
 		public ServiceHandler(Looper looper) {
-	          super(looper);
-	          Log.i(TAG, "ServiceHandler()");
-	      }
-	      @Override
-	      public void handleMessage(Message msg) {
-	          Log.i(TAG, "handleMessage()");
-	          // Normally we would do some work here, like download a file.
-	          // For our sample, we just sleep for 5 seconds.
-	          long endTime = System.currentTimeMillis() + 5*1000;
-	          while (System.currentTimeMillis() < endTime) {
-	              synchronized (this) {
-	                  try {
-	                      wait(endTime - System.currentTimeMillis());
-	                  } catch (Exception e) {
-	                  }
-	              }
-	          }
-	          // Stop the service using the startId, so that we don't stop
-	          // the service in the middle of handling another job
-//	          stopSelf(msg.arg1);
-	      }
-	  }
+			super(looper);
+			Log.i(TAG, "ServiceHandler()");
+		}
 
-	  @Override
-	  public void onCreate() {
-          Log.i(TAG, "onCreate()");
-	    // Start up the thread running the service.  Note that we create a
-	    // separate thread because the service normally runs in the process's
-	    // main thread, which we don't want to block.  We also make it
-	    // background priority so CPU-intensive work will not disrupt our UI.
-	    HandlerThread thread = new HandlerThread("ServiceStartArguments");
-	    thread.start();
-	    
-	    // Get the HandlerThread's Looper and use it for our Handler 
-	    mServiceLooper = thread.getLooper();
-	    mServiceHandler = new ServiceHandler(mServiceLooper);
-	    
-	    // Register the LRT receiver
-	    IntentFilter filter = new IntentFilter(ACTION_LRT_TEST);
-	    registerReceiver(mLrtReceiver, filter);
-	    
-	    setNotification("LRT Tracing Service Running", true);
-	  }
-
-	  @Override
-	  public int onStartCommand(Intent intent, int flags, int startId) {
-          Log.i(TAG, "onStartCommand()");
-	      Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
-	      
-	      // If we get killed, after returning from here, restart
-	      return START_STICKY;
-	  }
-
-	  @Override
-	  public IBinder onBind(Intent intent) {
-          Log.i(TAG, "onBind()");
-	      // We don't provide binding, so return null
-	      return null;
-	  }
-	  
-	  @Override
-	  public void onDestroy() {
-          Log.i(TAG, "onDestroy()");
-	    Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show(); 
-	    unregisterReceiver(mLrtReceiver);
-	    
-	  }
-	 
-
-	    /**
-	     * Listens for USB events, such as Disconnect, Permission to use, and data received
-	     * 
-	     * @category	Receiver
-	     * @category	USB
-	     */
-		private final BroadcastReceiver mLrtReceiver = new BroadcastReceiver() {
-		    public void onReceive(Context context, Intent intent) {
-				if (DEBUG) Toast.makeText(context, "LRT broadcast received", Toast.LENGTH_SHORT).show();
-		    	try {
-			    	if(intent != null) {
-			    		// Get the event that was broadcast
-				    	String action = intent.getAction();
-				    	String message = intent.getStringExtra("message");
-			    		Log.e(TAG, "GOT BROADCAST! MSG = "+message);
-			        	if (DEBUG)	Toast.makeText(context, "BROADCAST RCVD! "+action, Toast.LENGTH_SHORT).show();
-//				    	if (DEBUG) Toast.makeText(context, "LRT broadcast action: "+action, Toast.LENGTH_SHORT).show();
-				    	setNotification("LRT broadcast message: "+message, false);
-				        if(action == null) {
-				        	// Invalid action, exit
-				        	if (DEBUG)	Toast.makeText(context, "LRT - No action given", Toast.LENGTH_SHORT).show();
-				        	return;
-				        }
-			    	} else {
-				    	if (DEBUG) Toast.makeText(context, "LRT - No intent given", Toast.LENGTH_SHORT).show();
-			    	}
-				} catch (Exception e) {
-					Log.e(TAG, "Error: "+e);
+		@Override
+		public void handleMessage(Message msg) {
+			Log.i(TAG, "handleMessage()");
+			// Normally we would do some work here, like download a file.
+			// For our sample, we just sleep for 5 seconds.
+			long endTime = System.currentTimeMillis() + 5 * 1000;
+			while (System.currentTimeMillis() < endTime) {
+				synchronized (this) {
+					try {
+						wait(endTime - System.currentTimeMillis());
+					} catch (Exception e) {
+					}
 				}
-		    }
-		};
-		
-		private void setNotification(String msg, boolean ongoing) {
-			NotificationManager notificationManager =
-				    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-				int icon = R.drawable.ic_launcher;
-				CharSequence notiText = msg;
-				long meow = System.currentTimeMillis();
-
-				Notification notification = new Notification(icon, notiText, meow);
-
-				Context context = getApplicationContext();
-				CharSequence contentTitle = "LRT notification";
-				CharSequence contentText = msg;
-				Intent notificationIntent = new Intent(this, MainActivity.class);
-				PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-
-				notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
-
-				int SERVER_DATA_RECEIVED = 1;
-				if(ongoing) {
-					notification.flags |= Notification.FLAG_ONGOING_EVENT;
-					notification.flags |= Notification.FLAG_SHOW_LIGHTS;
-				    startForeground(1337, notification);
-				} else {
-					notificationManager.notify(SERVER_DATA_RECEIVED, notification);
-				}
+			}
+			// Stop the service using the startId, so that we don't stop
+			// the service in the middle of handling another job
+			// stopSelf(msg.arg1);
 		}
 	}
+
+	@Override
+	public void onCreate() {
+		Log.i(TAG, "onCreate()");
+		// Start up the thread running the service. Note that we create a
+		// separate thread because the service normally runs in the process's
+		// main thread, which we don't want to block. We also make it
+		// background priority so CPU-intensive work will not disrupt our UI.
+		HandlerThread thread = new HandlerThread("ServiceStartArguments");
+		thread.start();
+
+		// Get the HandlerThread's Looper and use it for our Handler
+		mServiceLooper = thread.getLooper();
+		mServiceHandler = new ServiceHandler(mServiceLooper);
+
+		// Register the LRT receiver
+		IntentFilter filter = new IntentFilter(ACTION_LRT_TEST);
+		registerReceiver(mLrtReceiver, filter);
+
+		setNotification("LRT Tracing Service Running", true);
+
+		infiLoop();
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Log.i(TAG, "onStartCommand()");
+		Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+
+		// If we get killed, after returning from here, restart
+		return START_STICKY;
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		Log.i(TAG, "onBind()");
+		// We don't provide binding, so return null
+		return null;
+	}
+
+	@Override
+	public void onDestroy() {
+		Log.i(TAG, "onDestroy()");
+		Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
+		unregisterReceiver(mLrtReceiver);
+
+	}
+
+	private void infiLoop() {
+		Log.i(TAG, "Loop...");
+
+		getQueue();
+
+		Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			public void run() {
+				// runs a method every 30s
+				infiLoop();
+			}
+		}, 10000);
+	}
+
+	/**
+	 * Listens for USB events, such as Disconnect, Permission to use, and data
+	 * received
+	 * 
+	 * @category Receiver
+	 * @category USB
+	 */
+	private final BroadcastReceiver mLrtReceiver = new BroadcastReceiver() {
+		public void onReceive(Context context, Intent intent) {
+			// if (DEBUG) Toast.makeText(context, "LRT broadcast received",
+			// Toast.LENGTH_SHORT).show();
+			try {
+				if (intent != null) {
+					// Get the event that was broadcast
+					String action = intent.getAction();
+					String message = intent.getStringExtra("message");
+					Log.e(TAG, "MSG = " + message);
+					if (DEBUG)
+						Toast.makeText(context, "BROADCAST RCVD! " + action,
+								Toast.LENGTH_SHORT).show();
+					mDataBuffer.add(message);
+					Log.i(TAG, "Added msg to buffer: " + message);
+					Log.v(TAG, "Buffer size: " + mDataBuffer.size());
+
+					setNotification(message, false);
+					if (action == null) {
+						// Invalid action, exit
+						if (DEBUG)
+							Toast.makeText(context, "LRT - No action given",
+									Toast.LENGTH_SHORT).show();
+						return;
+					}
+				} else {
+					if (DEBUG)
+						Toast.makeText(context, "LRT - No intent given",
+								Toast.LENGTH_SHORT).show();
+				}
+			} catch (Exception e) {
+				Log.e(TAG, "Error: " + e);
+			}
+		}
+	};
+
+	private void setNotification(String msg, boolean ongoing) {
+		NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		int icon = R.drawable.ic_launcher;
+		CharSequence notiText = msg;
+		long meow = System.currentTimeMillis();
+
+		Notification notification = new Notification(icon, notiText, meow);
+
+		Context context = getApplicationContext();
+		CharSequence contentTitle = "LRT notification";
+		CharSequence contentText = msg;
+		Intent notificationIntent = new Intent(this, MainActivity.class);
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+				notificationIntent, 0);
+
+		notification.setLatestEventInfo(context, contentTitle, contentText,
+				contentIntent);
+
+		int SERVER_DATA_RECEIVED = 1;
+		if (ongoing) {
+			notification.flags |= Notification.FLAG_ONGOING_EVENT;
+			notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+			startForeground(1337, notification);
+		} else {
+			notificationManager.notify(SERVER_DATA_RECEIVED, notification);
+		}
+	}
+
+//	public static LinkedList<String> getQueue() {
+////		Log.i(TAG, mDataBuffer.toString());
+//		Log.v(TAG, "Dump queue | size = " + mDataBuffer.size());
+//		return mDataBuffer;
+//	}
+
+	public void dumpQueue() {
+		Log.v(TAG, "Dump queue | size = " + mDataBuffer.size());
+		
+//		while(mDataBuffer.size() > 0) {
+//			String item = mDataBuffer.remove();
+//			Log.v(TAG, " '--> "+item);
+//		}
+		Log.v(TAG, "[====/dump====]");
+	}
+}

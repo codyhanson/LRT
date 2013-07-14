36dc2ca3deaa43ad205ac25cac9d9a1b23888c5c
Cody Hanson <cody.hanson@flukenetworks.com>
2013-07-14 13:15:46 -0600

    added some body to the route functions. untested.

--- LRTServer/app.js
+++ LRTServer/app.js
@@ -20,11 +20,9 @@ var TracePoint = mongoose.model('TracePoint',tracePointSchema);
 //object model for Trace
 var traceSchema = new mongoose.Schema(
 {
-    osVersion: String,
-    osType : String,
+    os: {type:String, version:String},
     userId : String,
-    appName: String,
-    appVersion: String,
+    app: {name: String, version:String},
     traceServiceVersion: String
 }
 );
@@ -60,8 +58,16 @@ app.get('/', function(req,res) {
  * Create a new trace for a particular user
  */
 app.post('/users/:userId/traces', function(req,res) {
+    var newTrace = new Trace(req.body);
+    newTrace.userId = req.params.userId;
 
-
+    newTrace.save(function(err) {
+        if (err) {
+            sendResponse(res,503,{error: 'fail'});
+        } else {
+            sendResponse(res,201,newTrace.toObject();
+        }
+    }
 });
 
 /**
@@ -81,7 +87,25 @@ app.post('/traces/:traceId/tracepoints', function(req,res) {
  *
  */
 app.get('/users/:userId/traces', function (req,res) {
+//merge the userid from the REST path into the query hash.
+    var qs = req.query;
+    qs["userId"] = req.params.userId;
 
+    //the base query
+    //all elements left in qs will tell mongo to match the document field exactly.
+    //or for the date entry, it will match a range
+    var query = Trace.find(qs);
+
+    //execute the built up query
+    //use the lean() option to return json objects, not heavyweight mongoose objects
+    query.lean().exec(function(err,traces) {
+        if (err){
+            console.log("error with get list of traces");
+            sendResponse(res,503,{error:"query failed"});
+        } else {
+            sendResponse(res,200,traces);
+        }
+    });
 });
 
 /*
@@ -89,7 +113,16 @@ app.get('/users/:userId/traces', function (req,res) {
  *
  */
 app.get('/traces/:traceId', function (req,res) {
-
+    Trace.findOne({_id: :traceId}, function (err, trace) {
+        if (err) {
+            console.log(err);
+            sendResponse(res,503,{error: 'server error'});
+        } else if (trace == null) {
+            sendResponse(res,404,{error:'not found'});
+        } else {
+            sendResponse(res,200,trace.toObject());
+        }
+    });
 });
 
 /*
@@ -97,15 +130,54 @@ app.get('/traces/:traceId', function (req,res) {
  */
 app.get('/traces/:traceId/tracepoints', function (req,res) {
     //TODO: we might want to support pagination for this one.
+    //merge the userid from the REST path into the query hash.
+    var qs = req.query;
+    qs["traceId"] = req.params.traceId;
 
+    //the base query
+    //all elements left in qs will tell mongo to match the document field exactly.
+    var query = Trace.find(qs);
+
+    //execute the built up query
+    //use the lean() option to return json objects, not heavyweight mongoose objects
+    query.lean().exec(function(err,tracePoints) {
+        if (err){
+            console.log("error with get list of tracepoints");
+            sendResponse(res,503,{error:"query failed"});
+        } else {
+            sendResponse(res,200,tracePoints);
+        }
+    });
 });
 
 /*
  * get a tracepoint by id
  */
 app.get('/tracespoints/:tracePointId', function (req,res) {
-
+    TracePoint.findOne({_id: :tracePointId}, function (err, tp) {
+        if (err) {
+            console.log(err);
+            sendResponse(res,503,{error: 'server error'});
+        } else if (tp == null) {
+            sendResponse(res,404,{error:'not found'});
+        } else {
+            sendResponse(res,200,tp.toObject());
+        }
+    });
 });
+
+//delete a trace and all of the tracepoints that it owns.
+app.delete('/traces/:traceId', function (req,res) {
+
+}
+
+
+function sendResponse(res,statusCode,responseBodyObject) {
+    res.writeHead(statusCode,  {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(responseBody));
+    res.end();
+    console.log("Response: " + statusCode + ' body:' + responseBody);
+}
 
 
 app.listen(app.get('port'));
